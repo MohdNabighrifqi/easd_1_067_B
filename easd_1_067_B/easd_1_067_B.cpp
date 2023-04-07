3cc2f001ce3b77f985f041a5ecda1f30eef8aa75
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-04-07 11:01:00 +0700

    Add project files.

---
+++ easd_1_067_B/easd_1_067_B.cpp
@@ -0,0 +1,12 @@
+// 1. Karena Algoritma dapat, mengurutkan data, mencari rute terpendek pada peta, menghitung nilai dari suatu persamaan matematika, dan lain sebagainya. 
+//    Dengan menggunakan algoritma, proses pemecahan masalah menjadi lebih terstruktur, efisien, dan dapat diulang dengan hasil yang konsisten.
+// 2. ada dua yang pertama ada data linear dan 
+// 3. ukuran dari input, kompleksitas, implementasi 
+// 4.
+// 5. yang quadratic terdapat inserionsort, selctionsort, dan bubblesort dan pada log linear terdapat mergesort dan quicksort
+// 6.
+#include <iostream>
+using namespace std;
+
+const int MAX_DATA = 87 ; 
+const string INITIALS = "MR";
